8bee182751892899ba6d4e2ce5343c567f531889
Flavio Esteban Poblete Arteaga (flavio.poblete) <flavio.poblete@sc.uchile.dom>
2025-08-18 17:19:25 -0400

    dejar funcion de busqueda de dataframe con cache

--- dominios_data.py
+++ dominios_data.py
@@ -1,6 +1,7 @@
 
 import pandas as pd
 from pymongoarrow.monkey import patch_all
+import streamlit as st
 
 patch_all()
 
@@ -21,3 +22,15 @@ def obtener_df_mongodb(client, db, collection, dominio, subdominio):
     except:
        return pd.DataFrame()
     
+@st.cache_data(ttl=500)
+def obtener_df_mongodb_cached(_client, db, collection_data, selected_domain, selected_subdomain):
+    """
+    Función para obtener datos de MongoDB
+    """
+    return obtener_df_mongodb(
+        _client,
+        db,
+        collection_data,
+        selected_domain,
+        selected_subdomain
+    )
--- filtros.py
+++ filtros.py
@@ -10,7 +10,7 @@ pd.options.mode.copy_on_write = True
 def resetear_filtros(selected_domain, selected_subdomain):
     st.session_state.query_busqueda = ""
     st.session_state.valor_filtro = "Todos"
-    st.session_state.df_base = dominios_data.obtener_df_mongodb(
+    st.session_state.df_base = dominios_data.obtener_df_mongodb_cached(
         st.session_state.client, 
         st.session_state.db, 
         st.session_state.collection_data, 
@@ -19,6 +19,7 @@ def resetear_filtros(selected_domain, selected_subdomain):
     )
 
 def mostrar_filtros(selected_domain, selected_subdomain, client, db, collection_data):
+    # Inicio de estado
     if "df_base" not in st.session_state or \
        st.session_state.get("selected_domain") != selected_domain or \
        st.session_state.get("selected_subdomain") != selected_subdomain:
@@ -27,12 +28,13 @@ def mostrar_filtros(selected_domain, selected_subdomain, client, db, collection_
         st.session_state.selected_subdomain = selected_subdomain
         st.session_state.query_busqueda = ""
         st.session_state.valor_filtro = "Todos"
-        st.session_state.df_base = dominios_data.obtener_df_mongodb(client, db, collection_data, selected_domain, selected_subdomain)
+        st.session_state.df_base = dominios_data.obtener_df_mongodb_cached(client, db, collection_data, selected_domain, selected_subdomain)
 
     st.session_state.client = client
     st.session_state.db = db
     st.session_state.collection_data = collection_data
-
+    
+    # Dataframe que trae cuando se selecciona el dominio y subdominio
     df_to_display = st.session_state.df_base
     
     if df_to_display.empty:
@@ -41,6 +43,7 @@ def mostrar_filtros(selected_domain, selected_subdomain, client, db, collection_
     
     st.subheader(f"Datos de {selected_subdomain}")
 
+    # Campo de busqueda
     query_busqueda = st.text_input("Ingresa tu búsqueda aquí:", value=st.session_state.query_busqueda, key="input_busqueda")
 
     button_col1, button_col3 = st.columns(2) 
@@ -61,18 +64,24 @@ def mostrar_filtros(selected_domain, selected_subdomain, client, db, collection_
     elif st.session_state.query_busqueda:
         df_to_display = buscador.busqueda_dataframe(df_to_display, st.session_state.query_busqueda)
 
+    # Filtrado
     if "Objeto de Datos" in df_to_display.columns:
         objetos_opciones = ["Todos"] + df_to_display["Objeto de Datos"].unique().tolist()
         
+        # Valor seleccionado aún exista en las opciones
+        selected_index = 0
+        if st.session_state.valor_filtro in objetos_opciones:
+            selected_index = objetos_opciones.index(st.session_state.valor_filtro)
+
         filtro_seleccionado = st.selectbox(
-            'Filtra por valor en "Objeto de Datos"', 
-            options=objetos_opciones, 
-            index=objetos_opciones.index(st.session_state.valor_filtro),
-            key=f"objeto_filter_selectbox_{selected_domain}_{selected_subdomain}"
+            'Filtra por valor en "Objeto de Datos"',
+            options=objetos_opciones,
+            index=selected_index,
+            key=f"objeto_filter_selectbox_{selected_domain}_{selected_subdomain}_{st.session_state.query_busqueda}"
         )
-        
         st.session_state.valor_filtro = filtro_seleccionado
-        
+
+        # Se aplica el filtro al DataFrame después de la búsqueda
         if st.session_state.valor_filtro != "Todos":
             df_to_display = df_to_display[df_to_display["Objeto de Datos"] == st.session_state.valor_filtro]
             if df_to_display.empty:
@@ -80,6 +89,8 @@ def mostrar_filtros(selected_domain, selected_subdomain, client, db, collection_
     else:
         st.info("La columna 'Objeto de Datos' no está disponible para aplicar filtros en este subdominio o no hay datos después de la búsqueda.")
 
+ 
+    # AgGrid
     if not df_to_display.empty:
         gb = GridOptionsBuilder.from_dataframe(df_to_display)
         gb.configure_default_column(wrapText=True, autoHeight=True)
